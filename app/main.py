6ee30c046b0e532a444a387d2a7debce313e6275
julscatalan <julscatalan@gmail.com>
2025-11-30 14:00:36 -0600

    add startup for app

--- app/main.py
+++ app/main.py
@@ -3,14 +3,33 @@ from fastapi import FastAPI, Request, HTTPException
 from dotenv import load_dotenv
 import uvicorn
 load_dotenv()
+
 from .db import init_db, count_embeddings
 from .loader import load_products_to_db
 from app.models import ConversationalResponse
 from kapso.use_kapso import use_kapso
 
 
-
 app = FastAPI()
+
+@app.on_event("startup")
+def startup_event():
+    print("📦 Inicializando base de datos...")
+    init_db()
+
+    existing = count_embeddings()
+
+    if existing == 0:
+        print("📤 No hay embeddings. Cargando datos y generando embeddings...")
+        csv_path = "app/data/products.csv"
+        load_products_to_db(csv_path)
+        print("✅ Datos cargados correctamente.")
+    else:
+        print(f"🔍 Embeddings existentes detectados: {existing}. No se recargarán datos.")
+
+@app.get("/")
+def root():
+    return {"status": "API funcionando"}
 
 @app.post("/whatsapp" )
 async def whatsapp_agent(request: Request):
