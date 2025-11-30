d0592b016318615803a115e7490515046677c638
Tavera <95766747+ATaveraMi@users.noreply.github.com>
2025-11-30 12:56:15 -0600

    feat: initial setup

---
+++ app/main.py
@@ -0,0 +1,13 @@
+"""Main module for the FastAPI application."""
+from fastapi import FastAPI
+import uvicorn
+
+
+
+app = FastAPI()
+
+@app.get("/ping")
+def ping():
+    return {"message": "pong"}
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
